de233f44cc2498506cb8171e415c2a6aa6379bbd
Smitfall <captainfalconrsmith@gmail.com>
2022-01-30 16:47:43 -0700

    Will add outgoing message

--- BetterCalculator/BetterCalculator.vb
+++ BetterCalculator/BetterCalculator.vb
@@ -11,55 +11,122 @@ Option Compare Text
 Module BetterCalculator
 
     Sub Main()
+
         Dim userResponse As String
         Dim secondResponse As String
+        Dim firstNumber As Integer
+        Dim secondNumber As Integer
         Dim menu As String
-        Dim booleanSwitch As Boolean
+        Dim killSwitch As Boolean = False
         Dim result As Integer
+        Dim loopOne As Boolean = False
+        Dim loopTwo As Boolean = False
+        Dim loopthree As Boolean = False
+
 
         Do
 
-            Console.WriteLine("Please enter two numbers. Enter ")
-                userResponse = Console.ReadLine()
-                Console.WriteLine($"You entered {userResponse} ")
-                Console.WriteLine("Please enter two numbers. Enter ")
-                secondResponse = Console.ReadLine
-                Console.WriteLine($"you entered {secondResponse}")
+            Do
+                Try
+                    Console.WriteLine("Please enter two numbers. Enter " & Chr(34) & "Q" & Chr(34) & "to quit")
+                    userResponse = Console.ReadLine()
+                    firstNumber = CInt(userResponse)
+                    Console.WriteLine($"You entered {userResponse} ")
+                    loopOne = False
+                Catch
+                    If userResponse = "Q" Then
+                        killSwitch = True
+                    Else
+                        Console.WriteLine($" you entered {userResponse} please enter a whole number")
+                        Console.ReadLine()
+                        loopOne = True
+                    End If
 
-                Console.WriteLine("1. Add" & vbNewLine _
+
+                End Try
+
+            Loop Until loopOne = False
+
+            If killSwitch <> True Then
+                Do
+                    Try
+                        Console.WriteLine("Please enter two numbers. Enter " & Chr(34) & "Q" & Chr(34) & "to quit")
+                        secondResponse = Console.ReadLine()
+                        secondNumber = CInt(secondResponse)
+                        Console.WriteLine($"You entered {secondResponse} ")
+                        loopTwo = False
+                    Catch
+                        If secondResponse = "Q" Then
+                            killSwitch = True
+                        Else
+                            Console.WriteLine($" you entered {secondResponse} please enter a whole number")
+                            Console.ReadLine()
+                            loopTwo = True
+                        End If
+
+
+                    End Try
+
+                Loop Until loopTwo = False
+
+            End If
+
+            Do
+                If killSwitch <> True Then
+
+                    Console.WriteLine("Choose one of the following option:" & vbNewLine _
+                                 & "1. Add" & vbNewLine _
                                  & "2. Subtract" & vbNewLine _
                                  & "3. Multiply" & vbNewLine _
                                  & "4. Divide")
-                menu = Console.ReadLine()
-                Console.WriteLine($" You entered {menu}")
+                    menu = Console.ReadLine()
+                    Console.WriteLine($" You entered {menu}")
 
-                Select Case menu
-                Case "1"
-                    result = CInt(userResponse) + CInt(secondResponse)
-                    Console.WriteLine($"{userResponse} + {secondResponse} = {result}")
-                    Console.ReadLine()
-                Case "2"
-                    result = CInt(userResponse) - CInt(secondResponse)
-                    Console.WriteLine($"{userResponse} - {secondResponse} = {result}")
-                    Console.ReadLine()
-                Case "3"
-                    result = CInt(userResponse) * CInt(secondResponse)
-                    Console.WriteLine($"{userResponse} * {secondResponse} = {result}")
-                    Console.ReadLine()
-                Case "4"
-                    result = CInt(userResponse) \ CInt(secondResponse)
-                    Console.Write($"{userResponse} \ {secondResponse} = {result} with a remainder of ")
-                    Console.WriteLine(CInt(userResponse) Mod CInt(secondResponse))
-                Case "Q"
-                    booleanSwitch = False
-                Case Else
-                    Console.WriteLine($"you entered {menu}")
-            End Select
+                    Try
+                        Select Case menu
+                            Case "1"
+                                result = firstNumber + secondNumber
+                                Console.WriteLine($"{firstNumber} + {secondNumber} = {result}")
+                                Console.ReadLine()
+                                loopthree = False
+                            Case "2"
+                                result = firstNumber - secondNumber
+                                Console.WriteLine($"{firstNumber} - {secondNumber} = {result}")
+                                Console.ReadLine()
+                                loopthree = False
+                            Case "3"
+                                result = firstNumber * secondNumber
+                                Console.WriteLine($"{firstNumber} * {secondNumber} = {result}")
+                                Console.ReadLine()
+                                loopthree = False
+                            Case "4"
+                                result = firstNumber \ secondNumber
+                                Console.Write($"{firstNumber} \ {secondNumber} = {result} with a remainder of ")
+                                Console.WriteLine(firstNumber Mod secondNumber)
+                                loopthree = False
+                            Case "Q"
+                                killSwitch = True
+                                loopthree = False
+                            Case Else
+                                Console.WriteLine($"you entered {menu}")
+                                loopthree = True
+                        End Select
+                    Catch ex As Exception
+                        Console.WriteLine($"you entered {menu}")
+                    End Try
+
+                End If
+
+            Loop Until loopthree = False
 
 
+        Loop Until killSwitch = True
+        Console.WriteLine("you have entered " & Chr(34) & "Q" & Chr(34))
+        Console.WriteLine("Have a nice day!")
+        Console.WriteLine("Press enter to close this window")
 
-        Loop Until booleanSwitch = False
         Console.Read()
+
 
     End Sub
 
