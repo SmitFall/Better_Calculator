52451f94c9a8069a0977ff8c8f2c4205f71b154a
Smitfall <captainfalconrsmith@gmail.com>
2022-01-30 12:58:17 -0700

    Add project files.

---
+++ BetterCalculator/BetterCalculator.vb
@@ -0,0 +1,35 @@
+﻿'Fallon Smith
+'RCET0265
+'Spring 2022
+'Better Calculator
+'
+
+Option Explicit On
+Option Strict On
+Option Compare Text
+
+Module BetterCalculator
+
+    Sub Main()
+        Dim userResponse As String
+        Dim secondResponse As String
+        Dim menu As String
+        Dim booleanSwitch As Boolean
+        Dim message As String
+
+
+        Do
+
+            Console.WriteLine("Please enter two numbers. Enter ")
+            userResponse = Console.ReadLine()
+            Console.WriteLine($"You entered {userResponse} ")
+            Console.WriteLine("Please enter two numbers. Enter ")
+            secondResponse = Console.ReadLine
+            Console.WriteLine($"you entered {secondResponse}")
+
+        Loop
+        Console.Read()
+
+    End Sub
+
+End Module
